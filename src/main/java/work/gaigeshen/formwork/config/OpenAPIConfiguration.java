85a633b5e2e99d0d11c41d737f11a923162e7024
gaigeshen <gaigeshen@qq.com>
2022-09-26 13:59:33 +0800

    接口文档生成结合安全框架

--- src/main/java/work/gaigeshen/formwork/config/OpenAPIConfiguration.java
+++ src/main/java/work/gaigeshen/formwork/config/OpenAPIConfiguration.java
@@ -1,9 +1,13 @@
 package work.gaigeshen.formwork.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -11,22 +15,24 @@ import org.springframework.context.annotation.Configuration;
  *
  * @author gaigeshen
  */
+@SecuritySchemes({
+        @SecurityScheme(
+                type = SecuritySchemeType.APIKEY,
+                in = SecuritySchemeIn.HEADER,
+                name = "apiKey",
+                paramName = "X-Auth-Token")})
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Open API document",
+                description = "This is a sample Open API document",
+                version = "1.0.0-SNAPSHOT",
+                contact = @Contact(
+                        name = "gaigeshen",
+                        email = "gaigeshen@qq.com",
+                        url = "https://github.com/gaigeshen")),
+        security = {
+                @SecurityRequirement(name = "apiKey")
+        }
+)
 @Configuration
-public class OpenAPIConfiguration {
-
-    @Bean
-    public OpenAPI openAPI() {
-        Contact contact = new Contact()
-                .name("gaigeshen")
-                .email("gaigeshen@qq.com")
-                .url("https://github.com/gaigeshen");
-
-        Info info = new Info()
-                .title("Open API document")
-                .description("This is a sample Open API document")
-                .version("1.0.0-SNAPSHOT")
-                .contact(contact);
-
-        return new OpenAPI().info(info);
-    }
-}
+public class OpenAPIConfiguration { }
