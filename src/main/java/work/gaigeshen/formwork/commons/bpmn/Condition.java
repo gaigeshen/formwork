8206f2dc56ab25689b5fcbfbde86d35359aeaaab
gaigeshen <gaigeshen@qq.com>
2022-09-28 09:21:02 +0800

    业务流程条件表达式优化

--- src/main/java/work/gaigeshen/formwork/commons/bpmn/Condition.java
+++ src/main/java/work/gaigeshen/formwork/commons/bpmn/Condition.java
@@ -14,6 +14,6 @@ public interface Condition {
         if (Objects.isNull(expression)) {
             throw new IllegalArgumentException("expression cannot be null");
         }
-        return () -> expression;
+        return () -> "(" + expression + ")";
     }
 }
