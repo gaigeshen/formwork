45b0c4647e6e55e28dc379b29b533e229d086128
gaigeshen <gaigeshen@qq.com>
2022-09-30 15:06:44 +0800

    配置修正

--- src/main/java/work/gaigeshen/formwork/config/SecurityConfiguration.java
+++ src/main/java/work/gaigeshen/formwork/config/SecurityConfiguration.java
@@ -110,7 +110,7 @@ public class SecurityConfiguration {
 
         http.csrf().disable().cors().configurationSource(r -> {
             CorsConfiguration config = new CorsConfiguration();
-            config.setAllowCredentials(true);
+            config.setAllowCredentials(false);
             config.setAllowedMethods(Arrays.asList("GET", "POST"));
             config.setAllowedHeaders(Collections.singletonList(CorsConfiguration.ALL));
             config.setAllowedOrigins(Collections.singletonList(CorsConfiguration.ALL));
