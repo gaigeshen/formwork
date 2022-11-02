44a883a9421b17f5fd084ffac9bd36db89fd036f
gaigeshen <gaigeshen@qq.com>
2022-11-02 11:17:43 +0800

    添加缓存的支持

---
+++ src/main/java/work/gaigeshen/formwork/config/CacheConfiguration.java
@@ -0,0 +1,14 @@
+package work.gaigeshen.formwork.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author gaigeshen
+ */
+@EnableCaching
+@Configuration
+public class CacheConfiguration {
+
+}
