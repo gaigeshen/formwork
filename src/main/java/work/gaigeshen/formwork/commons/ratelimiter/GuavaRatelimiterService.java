c7b2f5ba6363a6219fbc82a603fd3c18a455174d
gaigeshen <gaigeshen@qq.com>
2022-11-16 16:21:59 +0800

    添加限流服务

---
+++ src/main/java/work/gaigeshen/formwork/commons/ratelimiter/GuavaRatelimiterService.java
@@ -0,0 +1,46 @@
+package work.gaigeshen.formwork.commons.ratelimiter;
+
+import com.google.common.util.concurrent.RateLimiter;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 这是默认的实现
+ *
+ * @author gaigeshen
+ * @see #create(double)
+ */
+public class GuavaRatelimiterService implements RatelimiterService {
+
+    private final Map<String, RateLimiter> rateLimiters = new ConcurrentHashMap<>();
+
+    private final double permitsPerSecond;
+
+    public GuavaRatelimiterService(double permitsPerSecond) {
+        if (permitsPerSecond <= 0) {
+            throw new IllegalArgumentException("permitsPerSecond");
+        }
+        this.permitsPerSecond = permitsPerSecond;
+    }
+
+    @Override
+    public void acquire(String key, int permits) {
+        if (Objects.isNull(key)) {
+            throw new IllegalArgumentException("key cannot be null");
+        }
+        if (permits <= 0) {
+            throw new IllegalArgumentException("permits is invalid");
+        }
+        rateLimiters.computeIfAbsent(key, k -> {
+            double permitsPerSecond = getPermitsPerSecond();
+            return RateLimiter.create(permitsPerSecond);
+        }).acquire(permits);
+    }
+
+    @Override
+    public double getPermitsPerSecond() {
+        return permitsPerSecond;
+    }
+}
