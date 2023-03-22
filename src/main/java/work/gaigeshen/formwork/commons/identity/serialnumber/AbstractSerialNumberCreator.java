7966f240eeb35dcc035548f8c31e843ba242a86d
gaigeshen <gaigeshen@qq.com>
2023-03-22 10:46:28 +0800

    序列号生成器优化

--- src/main/java/work/gaigeshen/formwork/commons/identity/serialnumber/AbstractSerialNumberCreator.java
+++ src/main/java/work/gaigeshen/formwork/commons/identity/serialnumber/AbstractSerialNumberCreator.java
@@ -1,4 +1,4 @@
-package work.gaigeshen.formwork.commons.identity;
+package work.gaigeshen.formwork.commons.identity.serialnumber;
 
 import java.util.HashSet;
 import java.util.Map;
@@ -6,7 +6,7 @@ import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
- * 抽象序列号生成器
+ * 抽象序列号生成器，此类维护当前有效的序列号状态
  *
  * @author gaigeshen
  */
@@ -16,41 +16,34 @@ public abstract class AbstractSerialNumberCreator implements SerialNumberCreator
 
     @Override
     public final String create(String prefix) {
-        if (Objects.isNull(prefix)) {
-            throw new IllegalArgumentException("prefix cannot be null");
-        }
-        String serialNumber = prefix + serialNumbers.computeIfAbsent(prefix, p -> {
-            expiresSerialNumbers();
+        String checkedPrefix = Objects.isNull(prefix) ? "SN" : prefix;
+        return wrapSerialNumber(checkedPrefix, serialNumbers.computeIfAbsent(checkedPrefix, p -> {
+            for (String current : new HashSet<>(serialNumbers.keySet())) {
+                if (expireNecessary(current)) {
+                    serialNumbers.remove(current);
+                }
+            }
             long capacity = getCapacity(p);
             long newCapacity = getNewCapacity(p, capacity);
             return new SerialNumber(p, capacity + 1, newCapacity);
-        }).getPositionValue();
-        return wrapSerialNumber(prefix, serialNumber);
-    }
-
-    private void expiresSerialNumbers() {
-        for (String prefix : new HashSet<>(serialNumbers.keySet())) {
-            if (expireNecessary(prefix)) {
-                serialNumbers.remove(prefix);
-            }
-        }
+        }).getPositionValue());
     }
 
     /**
      * 此方法用于包装生成的序列号
      *
-     * @param prefix 序列号前缀
-     * @param serialNumber 生成的序列号（包含序列号前缀）
+     * @param prefix 序列号前缀不会为空
+     * @param serialNumber 生成的序列号数值
      * @return 包装后的序列号
      */
-    protected String wrapSerialNumber(String prefix, String serialNumber) {
-        return serialNumber;
+    protected String wrapSerialNumber(String prefix, long serialNumber) {
+        return prefix + serialNumber;
     }
 
     /**
-     * 确认指定的序列号前缀是否需要执行过期操作
+     * 确认指定的序列号前缀是否需要执行过期操作，每当新的序列号前缀被维护的时候，才会调用此方法
      *
-     * @param prefix 序列号前缀
+     * @param prefix 序列号前缀不会为空
      * @return 是否需要执行过期操作
      */
     protected boolean expireNecessary(String prefix) {
@@ -60,7 +53,7 @@ public abstract class AbstractSerialNumberCreator implements SerialNumberCreator
     /**
      * 获取指定序列号的当前容量，对于相同的序列号前缀，此方法不会出现多线程同时访问的情况
      *
-     * @param prefix 序列号前缀
+     * @param prefix 序列号前缀不会为空
      * @return 序列号的当前容量
      */
     protected long getCapacity(String prefix) {
@@ -70,7 +63,7 @@ public abstract class AbstractSerialNumberCreator implements SerialNumberCreator
     /**
      * 获取指定序列号新的容量，当需要扩容的时候会被调用，对于相同的序列号前缀，此方法不会出现多线程同时访问的情况
      *
-     * @param prefix 序列号前缀
+     * @param prefix 序列号前缀不会为空
      * @param oldCapacity 旧的容量
      * @return 新的容量
      */
@@ -114,7 +107,9 @@ public abstract class AbstractSerialNumberCreator implements SerialNumberCreator
 
         @Override
         public boolean equals(Object o) {
-            if (this == o) return true;
+            if (this == o) {
+                return true;
+            }
             if (o == null || getClass() != o.getClass()) {
                 return false;
             }
--- src/main/java/work/gaigeshen/formwork/commons/identity/serialnumber/SerialNumberCreator.java
+++ src/main/java/work/gaigeshen/formwork/commons/identity/serialnumber/SerialNumberCreator.java
@@ -1,4 +1,4 @@
-package work.gaigeshen.formwork.commons.identity;
+package work.gaigeshen.formwork.commons.identity.serialnumber;
 
 /**
  * 序列号生成器
@@ -10,17 +10,17 @@ public interface SerialNumberCreator {
     /**
      * 直接调用此方法生成默认的序列号
      *
-     * @param prefix 序列号对应的前缀
+     * @param prefix 序列号前缀应该支持可以为空
      * @return 生成的序列号
      */
     static String createDefault(String prefix) {
-        return DefaultSerialNumberCreator.INSTANCE.create(prefix);
+        return SimpleSerialNumberCreator.INSTANCE.create(prefix);
     }
 
     /**
      * 生成序列号
      *
-     * @param prefix 序列号对应的前缀
+     * @param prefix 序列号前缀应该支持可以为空
      * @return 生成的序列号
      */
     String create(String prefix);
