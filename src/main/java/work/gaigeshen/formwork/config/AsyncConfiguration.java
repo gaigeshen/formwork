7e071758bcf35cd995dc3712180a022e3c3195e4
gaigeshen <gaigeshen@qq.com>
2023-04-25 18:15:51 +0800

    添加任务异步执行的支持

---
+++ src/main/java/work/gaigeshen/formwork/config/AsyncConfiguration.java
@@ -0,0 +1,35 @@
+package work.gaigeshen.formwork.config;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+/**
+ *
+ * @author gaigeshen
+ */
+@EnableAsync
+@Configuration
+public class AsyncConfiguration implements AsyncConfigurer {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(10);
+        taskExecutor.setMaxPoolSize(30);
+        taskExecutor.setQueueCapacity(10);
+        taskExecutor.setKeepAliveSeconds(5);
+        taskExecutor.setThreadNamePrefix("async-task-");
+        taskExecutor.initialize();
+        return taskExecutor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return null;
+    }
+}
