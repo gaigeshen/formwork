43cf6256a7b1315d517a488b1242d4ef3b915256
gaigeshen <gaigeshen@qq.com>
2023-04-25 13:52:18 +0800

    添加拦截器用于将发送的请求体加密

---
+++ src/main/java/work/gaigeshen/formwork/security/web/crypto/CryptoHttpRequestInterceptor.java
@@ -0,0 +1,46 @@
+package work.gaigeshen.formwork.security.web.crypto;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+import work.gaigeshen.formwork.commons.json.JsonCodec;
+import work.gaigeshen.formwork.security.crypto.CryptoProcessor;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.GeneralSecurityException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 此拦截器用于将发送的请求体加密
+ *
+ * @author gaigeshen
+ */
+public class CryptoHttpRequestInterceptor implements ClientHttpRequestInterceptor {
+
+    private final CryptoProcessor cryptoProcessor;
+
+    public CryptoHttpRequestInterceptor(CryptoProcessor cryptoProcessor) {
+        this.cryptoProcessor = cryptoProcessor;
+    }
+
+    @Override
+    public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+        if (body.length == 0) {
+            return execution.execute(request, body);
+        }
+        String bodyString = new String(body, StandardCharsets.UTF_8);
+        String encrypted;
+        try {
+            encrypted = cryptoProcessor.doEncrypt(bodyString);
+        } catch (GeneralSecurityException e) {
+            throw new IOException(e.getMessage(), e);
+        }
+        Map<String, Object> bodyToSend = new HashMap<>();
+        bodyToSend.put("data", encrypted);
+        String encoded = JsonCodec.instance().encode(bodyToSend);
+        return execution.execute(request, encoded.getBytes(StandardCharsets.UTF_8));
+    }
+}
