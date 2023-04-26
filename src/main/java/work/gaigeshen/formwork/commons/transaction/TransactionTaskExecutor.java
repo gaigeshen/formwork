c28dcd3590112f84b144f95fb018ba1aacd544a6
gaigeshen <gaigeshen@qq.com>
2023-04-26 18:06:09 +0800

    注释优化

--- src/main/java/work/gaigeshen/formwork/commons/transaction/TransactionTaskExecutor.java
+++ src/main/java/work/gaigeshen/formwork/commons/transaction/TransactionTaskExecutor.java
@@ -12,7 +12,7 @@ public interface TransactionTaskExecutor {
     /**
      * 批量执行任务，会等待所有的任务执行完成
      *
-     * @param tasks 事务任务集合
+     * @param tasks 事务任务集合，事务任务抛出的异常不会被此方法再次抛出，而是会被包装为事务任务结果中的异常
      * @return 所有任务的执行结果，顺序同传入的任务
      * @throws Exception 在等待所有任务执行完成过程中发生异常
      */
