4f383ca943f713af5bfe28a2d8a514d70193b7ca
gaigeshen <gaigeshen@qq.com>
2023-04-28 13:17:20 +0800

    序列号生成器和标识符生成器配置

---
+++ src/main/java/work/gaigeshen/formwork/config/IdentityConfiguration.java
@@ -0,0 +1,28 @@
+package work.gaigeshen.formwork.config;
+
+import org.redisson.api.RedissonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import work.gaigeshen.formwork.commons.identity.IdentityGenerator;
+import work.gaigeshen.formwork.commons.identity.UUIDIdentityGenerator;
+import work.gaigeshen.formwork.commons.identity.serialnumber.RedissonSerialNumberGenerator;
+import work.gaigeshen.formwork.commons.identity.serialnumber.SerialNumberGenerator;
+
+/**
+ * 序列号生成器和标识符生成器配置
+ *
+ * @author gaigeshen
+ */
+@Configuration
+public class IdentityConfiguration {
+
+    @Bean
+    public SerialNumberGenerator serialNumberGenerator(RedissonClient redissonClient) {
+        return new RedissonSerialNumberGenerator(redissonClient);
+    }
+
+    @Bean
+    public IdentityGenerator identityGenerator() {
+        return UUIDIdentityGenerator.INSTANCE;
+    }
+}
