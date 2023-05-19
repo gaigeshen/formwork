ee6e53d673e3ff2a72ae24c794857b980aeb3c64
gaigeshen <gaigeshen@qq.com>
2023-05-19 10:30:28 +0800

    临时提交

--- src/main/java/work/gaigeshen/formwork/controller/LoggingFilter.java
+++ src/main/java/work/gaigeshen/formwork/controller/LoggingFilter.java
@@ -7,14 +7,18 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.stereotype.Component;
-import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -37,7 +41,7 @@ public class LoggingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper((HttpServletRequest) request);
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
         ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper((HttpServletResponse) response);
 
         String traceId = UUID.randomUUID().toString().replace("-", "");
@@ -45,15 +49,18 @@ public class LoggingFilter implements Filter {
 
         MDC.put(TRACE_ID_KEY, traceId);
 
-        log.info("URI => {} {}", requestWrapper.getMethod(), requestWrapper.getRequestURI());
-        log.info("Query => {}", requestWrapper.getQueryString());
+        log.info("URI => {} {}", httpRequest.getMethod(), httpRequest.getRequestURI());
+        log.info("Query => {}", httpRequest.getQueryString());
 
-        log.info("Headers => {}", new ServletServerHttpRequest(requestWrapper).getHeaders());
+        log.info("Headers => {}", new ServletServerHttpRequest(httpRequest).getHeaders());
 
-        log.info("Body => {}", getBody(requestWrapper));
+        ByteArrayOutputStream bodyBytes = new ByteArrayOutputStream();
+        StreamUtils.copy(httpRequest.getInputStream(), bodyBytes);
+
+        log.info("Body => {}", getBody(httpRequest, bodyBytes.toByteArray()));
 
         try {
-            chain.doFilter(requestWrapper, responseWrapper);
+            chain.doFilter(new RequestBodyHttpServletRequest(httpRequest, bodyBytes.toByteArray()), responseWrapper);
             log.info("Response => {}", getResponse(responseWrapper));
             responseWrapper.copyBodyToResponse();
         } finally {
@@ -61,10 +68,10 @@ public class LoggingFilter implements Filter {
         }
     }
 
-    private String getBody(ContentCachingRequestWrapper httpRequest) {
+    private String getBody(HttpServletRequest httpRequest, byte[] bodyBytes) {
         String contentType = httpRequest.getContentType();
         if (Objects.nonNull(contentType) && contentType.startsWith("application/json")) {
-            return new String(httpRequest.getContentAsByteArray());
+            return new String(bodyBytes);
         }
         return "null";
     }
@@ -75,5 +82,91 @@ public class LoggingFilter implements Filter {
             return new String(httpResponse.getContentAsByteArray());
         }
         return "null";
+    }
+
+    private static class RequestBodyHttpServletRequest extends HttpServletRequestWrapper {
+
+        private final byte[] bodyBytes;
+
+        public RequestBodyHttpServletRequest(HttpServletRequest request, byte[] bodyBytes) {
+            super(request);
+            this.bodyBytes = bodyBytes;
+        }
+
+        @Override
+        public ServletInputStream getInputStream() throws IOException {
+            return new RequestBodyInputStream(getRequest().getInputStream(), bodyBytes);
+        }
+    }
+
+    private static class RequestBodyInputStream extends ServletInputStream {
+
+        private final InputStream origin;
+
+        private final ByteArrayInputStream delegate;
+
+        public RequestBodyInputStream(InputStream origin, byte[] body) {
+            this.origin = origin;
+            this.delegate = new ByteArrayInputStream(body);
+        }
+
+        @Override
+        public boolean isFinished() {
+            return false;
+        }
+
+        @Override
+        public boolean isReady() {
+            return true;
+        }
+
+        @Override
+        public void setReadListener(ReadListener readListener) {
+        }
+
+        @Override
+        public int read() {
+            return delegate.read();
+        }
+
+        @Override
+        public int read(byte[] b, int off, int len) {
+            return delegate.read(b, off, len);
+        }
+
+        @Override
+        public int read(byte[] b) throws IOException {
+            return delegate.read(b);
+        }
+
+        @Override
+        public long skip(long n) {
+            return delegate.skip(n);
+        }
+
+        @Override
+        public int available() {
+            return delegate.available();
+        }
+
+        @Override
+        public void close() throws IOException {
+            origin.close();
+        }
+
+        @Override
+        public synchronized void mark(int readlimit) {
+            delegate.mark(readlimit);
+        }
+
+        @Override
+        public synchronized void reset() {
+            delegate.reset();
+        }
+
+        @Override
+        public boolean markSupported() {
+            return delegate.markSupported();
+        }
     }
 }
