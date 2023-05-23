c7352071fd3f9d0f1b4d3e88c032eead468d5e86
gaigeshen <gaigeshen@qq.com>
2023-05-23 19:02:15 +0800

    临时提交

--- src/main/java/work/gaigeshen/formwork/basal/security/web/logging/aop/LoggingAspect.java
+++ src/main/java/work/gaigeshen/formwork/basal/security/web/logging/aop/LoggingAspect.java
@@ -3,13 +3,38 @@ package work.gaigeshen.formwork.basal.security.web.logging.aop;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import work.gaigeshen.formwork.basal.security.web.logging.LoggingPrinter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Objects;
 
 @Aspect
 public class LoggingAspect {
 
-    @Around("@annotation(mapping)")
-    public Object doArount(ProceedingJoinPoint pjp, PostMapping mapping) throws Throwable {
+    private final LoggingPrinter printer;
+
+    public LoggingAspect(LoggingPrinter printer) {
+        this.printer = printer;
+    }
+
+    @Around("@annotation(org.springframework.web.bind.annotation.GetMapping) " +
+            "|| @annotation(org.springframework.web.bind.annotation.PostMapping)")
+    public Object doArount(ProceedingJoinPoint pjp) throws Throwable {
+
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+
+        if (Objects.isNull(requestAttributes) || !(requestAttributes instanceof ServletRequestAttributes)) {
+            return pjp.proceed();
+        }
+
+        HttpServletRequest httpRequest = ((ServletRequestAttributes) requestAttributes).getRequest();
+
+        HttpServletResponse httpResponse = ((ServletRequestAttributes) requestAttributes).getResponse();
+
 
 
         return pjp.proceed();
