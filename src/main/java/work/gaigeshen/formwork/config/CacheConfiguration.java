e1fafe4ee5b89fa474aae285c674950095e35ade
gaigeshen <gaigeshen@qq.com>
2023-06-08 14:31:05 +0800

    缓存配置修正

--- src/main/java/work/gaigeshen/formwork/config/CacheConfiguration.java
+++ src/main/java/work/gaigeshen/formwork/config/CacheConfiguration.java
@@ -1,11 +1,16 @@
 package work.gaigeshen.formwork.config;
 
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
+
+import java.util.Objects;
 
 /**
  *
@@ -14,6 +19,26 @@ import org.springframework.data.redis.serializer.RedisSerializer;
 @EnableCaching
 @Configuration
 public class CacheConfiguration {
+
+    @Bean
+    public RedisCacheConfiguration redisCacheConfiguration(CacheProperties cacheProperties) {
+        CacheProperties.Redis redisProperties = cacheProperties.getRedis();
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig();
+        config = config.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.json()));
+        if (Objects.nonNull(redisProperties.getTimeToLive())) {
+            config = config.entryTtl(redisProperties.getTimeToLive());
+        }
+        if (Objects.nonNull(redisProperties.getKeyPrefix())) {
+            config = config.prefixCacheNameWith(redisProperties.getKeyPrefix());
+        }
+        if (!redisProperties.isCacheNullValues()) {
+            config = config.disableCachingNullValues();
+        }
+        if (!redisProperties.isUseKeyPrefix()) {
+            config = config.disableKeyPrefix();
+        }
+        return config;
+    }
 
     @Bean
     public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory factory) {
