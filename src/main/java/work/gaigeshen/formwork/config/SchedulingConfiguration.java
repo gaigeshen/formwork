9db91227012cd12473b0d72880b09ded93bcbee1
gaigeshen <gaigeshen@qq.com>
2023-07-28 11:13:27 +0800

    加入定时任务的支持

---
+++ src/main/java/work/gaigeshen/formwork/config/SchedulingConfiguration.java
@@ -0,0 +1,14 @@
+package work.gaigeshen.formwork.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ *
+ * @author gaigeshen
+ */
+@EnableScheduling
+@Configuration
+public class SchedulingConfiguration {
+
+}
